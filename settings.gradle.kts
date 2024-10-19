486aabea0b29fc48d38e20e77a8b8121ce278afc
ﾑ丂んズﾑ刀 <108182589+oxidvip@users.noreply.github.com>
2024-10-19 21:18:59 +0330

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,17 +1,3 @@
-include(":plugin:api")
-
-val buildPlugin = System.getenv("BUILD_PLUGIN")
-when {
-    buildPlugin.isNullOrBlank() -> {
-        include(":plugin:hysteria2")
-    }
-    buildPlugin == "none" -> {
-    }
-    else -> {
-        include(":plugin:$buildPlugin")
-    }
-}
-
 include(":app")
 
 rootProject.name = "husi"
